8859df588819758b02507c30829744bd93a0c6f8
Sanders DeNardi <sdenardi@relsci.com>
2013-11-22 17:16:54 -0500

    update object definitions for leagues

--- nfl.js
+++ nfl.js
@@ -1,1 +1,21 @@
-exports.updateURL: "http://www.nfl.com/liveupdate/scorestrip/scorestrip.json";
+var NFLScore = {
+	updateURL: 'http://www.nfl.com/liveupdate/scorestrip/scorestrip.json',
+	parseRawGame: function(rawGame) {
+		var gameDate; //make date from rawGame data
+		var gameState; //make state from rawGame
+		this.gameId = rawGame.id;
+		this.state = gameState;
+		this.date = gameDate;
+		this.gameTime = rawGame.tsc;
+		this.awayTeam = rawGame.atn;
+		this.awayScore = rawGame.ats;
+		this.homeTeam = rawGame.htn;
+		this.homeScore = rawGame.hts;
+	},
+	makeGameLink: function() {
+		var linkStub = '';
+		return linkStub + this.gameId;
+	}
+};
+
+module.exports = NFLScore;
--- nhl.js
+++ nhl.js
@@ -1,21 +1,26 @@
-exports.updateURL = "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp";
-
-exports.makeGameInstance = function(rawGame) {
-	var gameDate; //make date from rawGame data
-	var gameState; //make state from rawGame
-	return {
-		gameId: rawGame.id,
-		state: gameState,
-		date: gameDate,
-		gameTime: rawGame.tsc,
-		awayTeam: rawGame.atn,
-		awayScore: rawGame.ats,
-		homeTeam: rawGame.htn,
-		homeScore: rawGame.hts
-	};
+var NHLScore = {
+	updateURL: 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp',
+	getGameArray: function(rawData) {
+		rawData = rawData.replace('loadScoreboard(','');
+		rawData = rawData.substring(0,(rawData.length-2));
+		return JSON.parse(rawData).games;
+	},
+	parseRawGame: function(rawGame) {
+		var gameDate; //make date from rawGame data
+		var gameState; //make state from rawGame
+		this.gameId = rawGame.id;
+		this.state = gameState;
+		this.date = gameDate;
+		this.gameTime = rawGame.tsc;
+		this.awayTeam = rawGame.atn;
+		this.awayScore = rawGame.ats;
+		this.homeTeam = rawGame.htn;
+		this.homeScore = rawGame.hts;
+	},
+	makeGameLink: function() {
+		var linkStub = 'http://www.nhl.com/gamecenter/en/icetracker?id=';
+		return linkStub + this.gameId;
+	}
 };
 
-exports.makeGameLink = function(game) {
-	var linkStub = "http://www.nhl.com/gamecenter/en/icetracker?id=";
-	return linkStub + game.gameId;
-};
+module.exports = NHLScore;
