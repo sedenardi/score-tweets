e93927445599713a2a97c23eb7c1f261682a0ed5
Sanders DeNardi <sedenardi@gmail.com>
2013-11-22 01:13:57 -0500

    add nhl object constructor + link methods

--- nhl.js
+++ nhl.js
@@ -1,3 +1,21 @@
-module.export = {
-	updateURL: "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp"
-}
+exports.updateURL = "http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp";
+
+exports.makeGameInstance = function(rawGame) {
+	var gameDate; //make date from rawGame data
+	var gameState; //make state from rawGame
+	return {
+		gameId: rawGame.id,
+		state: gameState,
+		date: gameDate,
+		gameTime: rawGame.tsc,
+		awayTeam: rawGame.atn,
+		awayScore: rawGame.ats,
+		homeTeam: rawGame.htn,
+		homeScore: rawGame.hts
+	};
+};
+
+exports.makeGameLink = function(game) {
+	var linkStub = "http://www.nhl.com/gamecenter/en/icetracker?id=";
+	return linkStub + game.gameId;
+};
