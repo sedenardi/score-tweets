d67f69e504acd12664b2fd5ea6f6d8974aeba710
Sanders DeNardi <sedenardi@gmail.com>
2013-11-23 18:13:59 -0500

    make ambiguous

--- score-tweets.js
+++ score-tweets.js
@@ -3,14 +3,16 @@ var file = require('fs');
 var NHLScore = require('./nhl.js');
 var NFLScore = require('./nfl.js')
 
+var league = Object.create(NHLScore);
+
 var rawResponse = '';
-var request = http.get(NFLScore.updateURL, function(res) {
+var request = http.get(league.updateURL, function(res) {
 	res.on('data', function(chunk) {
 		//console.log('Got %d bytes of data', chunk.length);
 		rawResponse += chunk;
 	});
 	res.on('end', function() {
-		var array = NFLScore.getGameArray(rawResponse);
+		var array = league.getGameArray(rawResponse);
 		console.log('Response: ' + JSON.stringify(array));
 	});
 }).on('error', function(e) {
