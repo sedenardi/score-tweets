3a3026b6966320c4cb86b668762e977c09d5a1c0
Sanders DeNardi <sedenardi@gmail.com>
2013-11-23 18:09:00 -0500

    change back main server file's functionality

--- score-tweets.js
+++ score-tweets.js
@@ -1,41 +1,18 @@
 var http = require('http');
 var file = require('fs');
 var NHLScore = require('./nhl.js');
+var NFLScore = require('./nfl.js')
 
-var getData = function() {
-	var rawResponse = '';
-	var request = http.get(NHLScore.updateURL, function(res) {
-		res.on('data', function(chunk) {
-			//console.log('Got %d bytes of data', chunk.length);
-			rawResponse += chunk;
-		});
-		res.on('end', function() {
-			var array = NHLScore.getGameArray(rawResponse);
-			toFile(array);
-			//console.log('Response: ' + JSON.stringify(array));
-		});
-	}).on('error', function(e) {
-		console.log('Error: ' + e.message);
+var rawResponse = '';
+var request = http.get(NFLScore.updateURL, function(res) {
+	res.on('data', function(chunk) {
+		//console.log('Got %d bytes of data', chunk.length);
+		rawResponse += chunk;
 	});
-
-	var toFile = function(data){
-		var s = JSON.stringify(data,null,2);
-		var d = new Date(); 
-		var dateString = d.getMonth().toString() + 
-			d.getDate().toString() + 
-			d.getFullYear().toString() + 
-			d.getHours().toString() + 
-			d.getMinutes().toString();
-		var fileName = './tests/automated/nhl-' + dateString + '.json';
-		file.writeFile(fileName, s, function(err) {
-			if(err) {
-				console.log(err);
-			} else {
-				console.log(fileName + ' save successfully.');
-			}
-		});
-	};
-};
-
-var loop = setInterval(getData, 300000);
-console.log('Created loop');
+	res.on('end', function() {
+		var array = NFLScore.getGameArray(rawResponse);
+		console.log('Response: ' + JSON.stringify(array));
+	});
+}).on('error', function(e) {
+	console.log('Error: ' + e.message);
+});
