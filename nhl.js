f3300f32fc85bd0f85f62e2b3656562ac6481a2d
Sanders DeNardi <sedenardi@gmail.com>
2013-11-25 22:02:42 -0500

    parse raw object into game object

--- nhl.js
+++ nhl.js
@@ -4,22 +4,24 @@ var NHLScore = {
 	getGameArray: function(rawData) {
 		rawData = rawData.replace('loadScoreboard(','');
 		rawData = rawData.substring(0,(rawData.length-2));
-		return JSON.parse(rawData).games;
+		var rawArray = JSON.parse(rawData).games;
+		var gameArray = [];
+		for (var i = 0; i < rawArray.length; i++) {
+			gameArray.push(this.parseRawGame(rawArray[i]));
+		}
+		return gameArray;
 	},
 	parseRawGame: function(rawGame) {
+		var game = { };
 		var date = new Date();
-		var arr = rawGame.ts.split(' ');
-		var dateArr = arr[arr.length-1].split('/');
-		d.setMonth(parseInt(dateArr[0])-1);
-		d.setDate(parseInt(dateArr[1]));
 		var gameState = '';
 		switch (rawGame.gs) {
 			case '1':
 				gameState = 'scheduled';
 				var arr = rawGame.ts.split(' ');
 				var dateArr = arr[arr.length-1].split('/');
-				d.setMonth(parseInt(dateArr[0])-1);
-				d.setDate(parseInt(dateArr[1]));
+				date.setMonth(parseInt(dateArr[0])-1);
+				date.setDate(parseInt(dateArr[1]));
 				break;
 			case '3':
 				if (rawGame.ts.indexOf('END') !== -1) {
@@ -39,22 +41,27 @@ var NHLScore = {
 				gameState = 'ended';
 				var arr = rawGame.ts.split(' ');
 				var dateArr = arr[arr.length-1].split('/');
-				d.setMonth(parseInt(dateArr[0])-1);
-				d.setDate(parseInt(dateArr[1]));
+				date.setMonth(parseInt(dateArr[0])-1);
+				date.setDate(parseInt(dateArr[1]));
 				break;
 		}
-		this.gameId = rawGame.id;
-		this.state = gameState;
-		this.date = date.toDateString();
-		this.gameTime = rawGame.tsc;
-		this.awayTeam = rawGame.atn;
-		this.awayScore = rawGame.ats;
-		this.homeTeam = rawGame.htn;
-		this.homeScore = rawGame.hts;
+		var timeString = rawGame.ts.split(' ');
+		game.gameId = rawGame.id;
+		game.state = gameState;
+		game.date = date.toDateString();
+		game.gameTime = timeString[0];
+		game.gamePeriod = timeString[1];
+		game.awayTeamCity = rawGame.atn;
+		game.awayTeamName = rawGame.atv;
+		game.awayScore = rawGame.ats;
+		game.homeTeamCity = rawGame.htn;
+		game.homeTeamName = rawGame.htv;
+		game.homeScore = rawGame.hts;
+		return game;
 	},
-	makeGameLink: function() {
+	makeGameLink: function(game) {
 		var linkStub = 'http://www.nhl.com/gamecenter/en/icetracker?id=';
-		return linkStub + this.gameId;
+		return linkStub + game.gameId;
 	}
 };
 
