f869b607fe83b0516f32417030c31d9f02a7dff7
Sanders DeNardi <sedenardi@gmail.com>
2013-11-25 21:05:09 -0500

    change nfl parse url and fields

--- nfl.js
+++ nfl.js
@@ -1,26 +1,38 @@
 var NFLScore = {
 	league: 'NFL',
-	updateURL: 'http://www.nfl.com/liveupdate/scorestrip/scorestrip.json',
+	updateURL: 'http://www.nfl.com/liveupdate/scorestrip/ss.json',
 	getGameArray: function(rawData) {
-		rawData = rawData.replace(/\,\,/g,',"",');
-		rawData = rawData.replace(/\,\,/g,',"",');
-		return JSON.parse(rawData).ss;
+		var s = JSON.parse(rawData);
+		for (var i = 0; i < s.gms.length; i++) {
+			s.gms[i].outer_w = s.w;
+			s.gms[i].outer_t = s.t;
+			s.gms[i].outer_y = s.y;
+			s.gms[i].outer_gd = s.gd;
+			s.gms[i].outer_bph = s.bph;
+		}
+		return s.gms;
 	},
 	parseRawGame: function(rawGame) {
 		var gameDate; //make date from rawGame data
 		var gameState; //make state from rawGame
-		this.gameId = rawGame.id;
+		this.gameId = rawGame.eid;
 		this.state = gameState;
 		this.date = gameDate;
+		this.season = rawGame.outer_y;
+		this.week = rawGame.outer_t + rawGame.outer_w;
 		this.gameTime = rawGame.tsc;
-		this.awayTeam = rawGame.atn;
-		this.awayScore = rawGame.ats;
-		this.homeTeam = rawGame.htn;
-		this.homeScore = rawGame.hts;
+		this.awayTeamCity = rawGame.v;
+		this.awayTeamName = rawGame.vnn;
+		this.awayScore = rawGame.vs;
+		this.homeTeamCity = rawGame.h;
+		this.homeTeamName = rawGame.hnn;
+		this.homeScore = rawGame.hs;
 	},
 	makeGameLink: function() {
-		var linkStub = '';
-		return linkStub + this.gameId;
+		var link = 'http://www.nfl.com/gamecenter/' +
+		gameId + '/' + season + '/' + week + '/' +
+		awayTeamName + '@' + homeTeamName;
+		return link;
 	}
 };
 
