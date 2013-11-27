443a987fbbec8f808df9cf313b8f99030da0b376
Sanders DeNardi <sedenardi@gmail.com>
2013-11-27 01:00:00 -0500

    detect instance changes

--- my_modules/nhl.js
+++ my_modules/nhl.js
@@ -25,16 +25,72 @@ var processArray = function(games) {
 	for (var i = 0; i < games.length; i++) {
 		console.log(league.leagueName + ': processing ' + games[i].GameID);
 		(function process(game){
-			db.exists('Select 1 from NHLChanges where GameID = ?', [game.GameID], function (res) {
-				if (res.exists) {
+			db.exists('Select 1 from NHLGameInstances where GameID = ?', [game.GameID], function (res) {
+				if (res) {
 					console.log(league.leagueName + ': ' + game.GameID + ' does exist');
+					getLastGameInstance(game, function(res) {
+						var changed = league.gameChanged(res, game);
+						if (changed) {
+							console.log(league.leagueName + ': ' + game.GameID + ' game changed');
+						} else {
+							console.log(league.leagueName + ': ' + game.GameID + ' game not changed');
+						}
+					});
 				} else {
 					console.log(league.leagueName + ': ' + game.GameID + ' does not exist');
+					insertGame(game, function() {
+						console.log(league.leagueName + ': Inserted ' + game.GameID);
+					});
 				}
 			});
 		}(games[i]));
 	}
 }
+
+var insertGame = function(game, next) {
+	var insertSql = 
+		'Insert into NHLGameInstances(GameID,Date,StateID,Time,\
+			Period,AwayTeamID,AwayScore,HomeTeamID,HomeScore)\
+		Select\
+			?,?,state.StateID,?,?,away.TeamID,?,home.TeamID,?\
+		from NHLStates state\
+			inner join NHLTeams away\
+				on LOWER(REPLACE(away.Name,\' \',\'\')) like ?\
+			inner join NHLTeams home\
+				on LOWER(REPLACE(home.Name,\' \',\'\')) like ?\
+		where state.State = ?;';
+	var inserts = [game.GameID, game.Date, game.Time, game.Period, game.AwayScore,
+		game.HomeScore, game.AwayTeamName, game.HomeTeamName, game.State];
+	db.query(insertSql, inserts, next);
+}
+
+var getLastGameInstance = function(game, next) {
+	var sql = 
+		'Select\
+			game.GameID\
+		,	game.Date\
+		,	state.State\
+		,	game.Time\
+		,	game.Period\
+		,	away.City as AwayTeamCity\
+		,	away.Name as AwayTeamName\
+		,	game.AwayScore\
+		,	home.City as HomeTeamCity\
+		,	home.Name as HomeTeamName\
+		,	game.HomeScore\
+		from NHLGameInstances game\
+			inner join NHLStates state\
+				on state.StateID = game.StateID\
+			inner join NHLTeams away\
+				on away.TeamID = game.AwayTeamID\
+			inner join NHLTeams home\
+				on home.TeamID = game.HomeTeamID\
+		where GameID = ? order by ?? desc limit 1;';
+	var inserts = [game.GameID, 'RecordedOn'];
+	db.query(sql, inserts, function(res) {
+		next(res[0]);
+	});
+};
 
 /***** EXPORTS *****/
 module.exports.league = league;
