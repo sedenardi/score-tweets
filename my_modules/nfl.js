a1a24bd3ca341e1d16325846907d402ce27059d6
Sanders DeNardi <sedenardi@gmail.com>
2013-11-28 15:25:41 -0500

    do double link parse for nfl

--- my_modules/nfl.js
+++ my_modules/nfl.js
@@ -1,41 +1,82 @@
-var NFLScore = {
+var http = require('http');
+
+var leagueInfo = {
 	leagueName: 'NFL',
 	updateURL: 'http://www.nfl.com/liveupdate/scorestrip/ss.json',
-	updateURL2: 'http://www.nfl.com/liveupdate/scorestrip/scorestrip.json',
-	getGameArray: function(rawData) {
-		var s = JSON.parse(rawData);
-		for (var i = 0; i < s.gms.length; i++) {
-			s.gms[i].outer_w = s.w;
-			s.gms[i].outer_t = s.t;
-			s.gms[i].outer_y = s.y;
-			s.gms[i].outer_gd = s.gd;
-			s.gms[i].outer_bph = s.bph;
-		}
-		return s.gms;
-	},
-	parseRawGame: function(rawGame) {
-		var gameDate; //make date from rawGame data
-		var gameState; //make state from rawGame
-		this.GameID = rawGame.eid;
-		this.state = gameState;
-		this.date = gameDate;
-		this.Year = rawGame.outer_y;
-		this.SeasonType = ;
-		this.Week = rawGame.outer_t + rawGame.outer_w;
-		this.gameTime = rawGame.tsc;
-		this.awayTeamCity = rawGame.v;
-		this.awayTeamName = rawGame.vnn;
-		this.awayScore = rawGame.vs;
-		this.homeTeamCity = rawGame.h;
-		this.homeTeamName = rawGame.hnn;
-		this.homeScore = rawGame.hs;
-	},
-	makeGameLink: function(game) {
-		var link = 'http://www.nfl.com/gamecenter/' +
-		game.GameID + '/' + game.Year + '/' + game.SeasonType + 
-		game.Week + '/' + awayTeamName + '@' + homeTeamName;
-		return link;
-	}
+	updateURL2: 'http://www.nfl.com/liveupdate/scorestrip/scorestrip.json'
 };
 
-module.exports = NFLScore;
+var getGameArray = function(next) {
+	var rawOuter = '';
+	var outerRequest = http.get(leagueInfo.updateURL, function(oRes) {
+		oRes.on('data', function(chunk) {
+			rawOuter += chunk;
+		});
+		oRes.on('end', function() {
+			var rawInner = '';
+			var innerRequest = http.get(leagueInfo.updateURL2, function(iRes) {
+				iRes.on('data', function(chunk) {
+					rawInner += chunk;
+				});
+				iRes.on('end', function() {
+					rawInner = rawInner.replace(/\,\,/g,',"",');
+					rawInner = rawInner.replace(/\,\,/g,',"",');
+					innerArray = JSON.parse(rawInner).ss;
+					var innerObj = { };
+					for (var i = 0; i < innerArray.length; i++) {
+						innerObj[innerArray[i][10]] = innerArray[i];
+					}
+					outerObj = JSON.parse(rawOuter);
+					for (var i = 0; i < outerObj.gms.length; i++) {
+						outerObj.gms[i].outer_w = outerObj.w;
+						outerObj.gms[i].outer_t = outerObj.t;
+						outerObj.gms[i].outer_y = outerObj.y;
+						outerObj.gms[i].outer_gd = outerObj.gd;
+						outerObj.gms[i].outer_bph = outerObj.bph;
+						outerObj.gms[i].score_array = innerObj[outerObj.gms[i].gsis];
+					}
+					next(outerObj.gms);
+				});
+			}).on('error', function(e) {
+				console.log('NFL inner http Error: ' + e.message);
+			});
+		});
+	}).on('error', function(e) {
+		console.log('NFL outer http Error: ' + e.message);
+	});
+};
+
+var parseRawGame = function(rawGame) {
+	var gameDate; //make date from rawGame data
+	var gameState; //make state from rawGame
+	this.GameID = rawGame.eid;
+	this.state = gameState;
+	this.date = gameDate;
+	this.Year = rawGame.outer_y;
+	this.SeasonType = '';
+	this.Week = rawGame.outer_t + rawGame.outer_w;
+	this.gameTime = rawGame.tsc;
+	this.awayTeamCity = rawGame.v;
+	this.awayTeamName = rawGame.vnn;
+	this.awayScore = rawGame.vs;
+	this.homeTeamCity = rawGame.h;
+	this.homeTeamName = rawGame.hnn;
+	this.homeScore = rawGame.hs;
+};
+
+var makeGameLink = function(game) {
+	var link = 'http://www.nfl.com/gamecenter/' +
+	game.GameID + '/' + game.Year + '/' + game.SeasonType + 
+	game.Week + '/' + awayTeamName + '@' + homeTeamName;
+	return link;
+};
+
+module.exports.leagueInfo = leagueInfo;
+module.exports.getGameArray = getGameArray;
+module.exports.parseRawGame = parseRawGame;
+//module.exports.gameChanged = gameChanged;
+//module.exports.gameChangeString = gameChangeString;
+module.exports.makeGameLink = makeGameLink;
+//module.exports.existsQuery = existsQuery;
+//module.exports.insertGameQuery = insertGameQuery;
+//module.exports.lastGameInstanceQuery = lastGameInstanceQuery;
