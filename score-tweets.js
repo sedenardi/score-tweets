632184defc04c4b6080afc73c7f176741539c9be
Sanders DeNardi <sedenardi@gmail.com>
2013-11-28 15:26:24 -0500

    do testing for nfl

--- score-tweets.js
+++ score-tweets.js
@@ -2,13 +2,18 @@ var file = require('fs')
 ,	db = require('./my_modules/db.js')
 ,	config = require('./my_modules/config.js')
 ,	league = require('./my_modules/league.js')
-,	NHL = require('./my_modules/nhl.js');
-
-var leagueNHL = Object.create(league);
-leagueNHL.setLeague(NHL);
+,	NHL = require('./my_modules/nhl.js')
+,	NFL = require('./my_modules/nfl.js');
 
 var leagues = [];
-leagues.push(leagueNHL);
+
+/*var leagueNHL = Object.create(league);
+leagueNHL.setLeague(NHL);
+leagues.push(leagueNHL);*/
+
+var leagueNFL = Object.create(league);
+leagueNFL.setLeague(NFL);
+leagues.push(leagueNFL);
 
 for (var i = 0; i < leagues.length; i++) {
 	leagues[i].startProcess();
