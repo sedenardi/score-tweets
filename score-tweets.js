1e7de10f9800d14ee8620eb62852eaa7ba3d967c
Sanders DeNardi <sedenardi@gmail.com>
2013-12-03 23:19:50 -0500

    add initial web server

--- score-tweets.js
+++ score-tweets.js
@@ -1,10 +1,10 @@
 var file = require('fs'),
   db = require('./my_modules/db.js'),
   config = require('./my_modules/config.js'),
-  leagueManagerModule = './my_modules/leagueManager.js',
+  LeagueManager = require('./my_modules/leagueManager.js').LeagueManager,
   NHL = require('./my_modules/nhl.js'),
   NFL = require('./my_modules/nfl.js'),
-  LeagueManager = require(leagueManagerModule).LeagueManager;
+  Web = require('./my_modules/web.js');
 
 var leagues = [];
 
@@ -15,15 +15,7 @@ var leagueNFL = new LeagueManager(NFL);
 leagues.push(leagueNFL);
 
 for (var i = 0; i < leagues.length; i++) {
-  leagues[i].startProcess();
+  //leagues[i].startProcess();
 }
 
-var interval;
-var end = function() {
-  for (var i = 0; i < leagues.length; i++) {
-    leagues[i].endProcess();
-  }
-  clearInterval(interval);
-}
-
-//interval = setInterval(end,25000);
+var web = new Web();
