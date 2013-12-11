c4cd29f225cc3a279746a51e63753ddd1fe3f1fd
Sanders DeNardi <sedenardi@gmail.com>
2013-12-11 00:25:22 -0500

    add game state helper (nhl only)

--- my_modules/web/helpers.js
+++ my_modules/web/helpers.js
@@ -1,3 +1,20 @@
 exports.json = function(obj) {
   return JSON.stringify(obj, null, 2);
 };
+
+exports.state = function(game) {
+  switch (game.State) {
+    case 'Overtime':
+    case 'Intermission':
+    case 'Progress':
+      return game.Time + ' ' + game.Period;
+    case 'Shootout':
+      return 'Shootout';
+    case 'Final':
+      if (game.Period) {
+        return 'Final ' + game.Period;
+      } else {
+        return 'Final';
+      }
+  }
+};
