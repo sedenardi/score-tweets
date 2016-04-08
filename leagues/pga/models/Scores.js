90be18e37e4eeb3cfccc692708d8893c28b33752
Sanders DeNardi <sedenardi@gmail.com>
2016-04-08 01:23:01 -0400

    limit golfers

--- leagues/pga/models/Scores.js
+++ leagues/pga/models/Scores.js
@@ -21,6 +21,7 @@ Scores.prototype.getPlayer = function(otherPlayer) {
 
 Scores.prototype.getChanges = function(prev) {
   return _(this.Players)
+    .filter(function(p) { return p.positionNum() <= 10; })
     .map(function(p) {
       var otherPlayer = prev.getPlayer(p);
       if (!otherPlayer) {
