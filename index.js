9d10f5f17cc9f03e4013e7195275720075a60ba9
Sanders DeNardi <sedenardi@gmail.com>
2016-04-10 20:53:04 -0400

    slight refactor

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ var PGA = require('./leagues/pga/index');
 module.exports = {
   pga: function(event, context) {
     PGA.web(context.done);
-  },
-  pgaTest: function(event, context) {
-    PGA.test(context.done);
   }
 };
--- leagues/pga/index.js
+++ leagues/pga/index.js
@@ -12,37 +12,45 @@ var moment = require('moment');
 var Scores = require('../../models/pga/Scores');
 var url = 'http://www.pgatour.com/data/r/014/leaderboard-v2.json';
 
-var prev = require('../../examples/pga/1460152977741.json');
+var fetchNextFromWeb = function() {
+  return request.get(url).then(function(res) {
+    var next = Scores.parse(res);
+    return Promise.resolve(next);
+  });
+};
 
-var prevObj = Scores.parse(prev);
-
-var compareAndSave = function(newObj) {
+var getFromDynamo = function() {
   return dynamo.get({TableName: 'Leagues', Key: {League: 'PGA'}}).then(function(res) {
     if (res.Item) {
       var timeSinceLast = moment.duration(moment().diff(res.Item.CreatedOn));
       if (timeSinceLast.asMinutes() >= 20) {
         console.log('Skipping because existing item is too old');
-        return Promise.resolve();
+        return Promise.resolve(null);
       }
       return zlib.gunzipAsync(res.Item.Scores).then(function(unzipped) {
         var scores = JSON.parse(unzipped);
         var oldObj = new Scores(scores);
-        var changes = newObj.getChanges(oldObj);
-        if (changes.length) {
-          console.log('Found ' + changes.length + ' changes.');
-        }
-        var tweets = _.map(changes, function(status) {
-          return twitter.post(status);
-        });
-        return Promise.all(tweets);
+        return Promise.resolve(oldObj);
       });
-      //Promise.resolve();
     } else {
-      return Promise.resolve();
+      return Promise.resolve(null);
     }
-  }).then(function() {
-    return zlib.gzipAsync(JSON.stringify(newObj));
-  }).then(function(zipped) {
+  });
+};
+
+var compareAndTweet = function(oldObj, newObj) {
+  var changes = newObj.getChanges(oldObj);
+  if (changes.length) {
+    console.log('Found ' + changes.length + ' changes.');
+  }
+  var tweets = _.map(changes, function(status) {
+    return twitter.post(status);
+  });
+  return Promise.all(tweets);
+};
+
+var saveNewObj = function(newObj) {
+  return zlib.gzipAsync(JSON.stringify(newObj)).then(function(zipped) {
     return dynamo.put({
       TableName: 'Leagues',
       Item: {
@@ -53,35 +61,29 @@ var compareAndSave = function(newObj) {
     });
   }).then(function() {
     console.log('New Item Saved');
-  }).catch(function(err) {
-    console.log(err);
-  });
-};
-
-var fetchNextFromWeb = function() {
-  return request.get(url).then(function(res) {
-    var next = Scores.parse(res);
-    return Promise.resolve(next);
+    return Promise.resolve();
   });
 };
 
 module.exports = {
   web: function(cb) {
-    fetchNextFromWeb().then(function(nextObj) {
+    var nextObj = null;
+    fetchNextFromWeb().then(function(res) {
+      nextObj = res;
       if (!nextObj) {
         console.log('Bad data from ' + url);
         return Promise.reject('Bad data from ' + url);
       }
-      return compareAndSave(nextObj);
+      return getFromDynamo();
+    }).then(function(oldObj) {
+      if (oldObj) {
+        return compareAndTweet(oldObj, nextObj);
+      } else {
+        return Promise.resolve();
+      }
     }).then(function() {
-      cb(null, 'done');
-    }).catch(function(err) {
-      console.log(err);
-      cb(err);
-    });
-  },
-  test: function(cb) {
-    compareAndSave(prevObj).then(function() {
+      return saveNewObj(nextObj);
+    }).then(function() {
       cb(null, 'done');
     }).catch(function(err) {
       console.log(err);
