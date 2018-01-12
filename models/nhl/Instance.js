4b76fa1c1708f8e4ccdfd8a1ecb32abf3cd23f28
Sanders DeNardi <sedenardi@gmail.com>
2018-01-11 22:19:30 -0500

    ES6-ify NHL class

--- models/nhl/Instance.js
+++ models/nhl/Instance.js
@@ -1,12 +1,74 @@
 'use strict';
 
-const Instance = function(instance) {
-  this.State = instance.State;
-  this.Period = instance.Period;
-  this.Time = instance.Time;
-  this.HomeScore = instance.HomeScore;
-  this.AwayScore = instance.AwayScore;
-};
+class Instance {
+  constructor(instance) {
+    this.State = instance.State;
+    this.Period = instance.Period;
+    this.Time = instance.Time;
+    this.HomeScore = instance.HomeScore;
+    this.AwayScore = instance.AwayScore;
+  }
+  scoreChanged(prevInstance) {
+    return (
+      prevInstance.State !== this.State ||
+      prevInstance.Period !== this.Period ||
+      prevInstance.AwayScore !== this.AwayScore ||
+      prevInstance.HomeScore !== this.HomeScore ||
+      (
+        prevInstance.State === 'Scheduled' &&
+        this.State === 'Scheduled' &&
+        prevInstance.Time !== this.Time &&
+        this.Time && this.Time.length
+      )
+    );
+  }
+  changeString(prevInstance, game) {
+    let str = '';
+    const scores = game.AwayTeam.Name + ' ' + this.AwayScore + ', ' +
+      game.HomeTeam.Name + ' ' + this.HomeScore + '. ';
+    if (prevInstance.State !== this.State) {
+      if (prevInstance.State === 'Scheduled' && this.State === 'Progress') {
+        if (this.Period !== '1st') {
+          return str;
+        }
+        str = 'Start of game: ' + game.AwayTeam.Name + ' vs ' + game.HomeTeam.Name;
+      }
+      if (this.State === 'Final') {
+        if (this.Period === 'SO') {
+          str = 'Final SO. ' + scores;
+        } else if (this.Period === 'OT') {
+          str = 'Final OT. ' + scores;
+        } else {
+          str = 'Final. ' + scores;
+        }
+      }
+    } else {
+      const isOT = (this.State === 'Overtime' || this.State === 'Shootout');
+      if (prevInstance.AwayScore > this.AwayScore ||
+        prevInstance.HomeScore > this.HomeScore) {
+        str = 'Score Correction. ' + scores + this.Time + ' ' + this.Period;
+      } else if (prevInstance.AwayScore !== this.AwayScore && !isOT) {
+        if (this.AwayScore === this.HomeScore) {
+          str = game.AwayTeam.Name + ' tie it up. ' + scores + this.Time + ' ' + this.Period;
+        } else if (prevInstance.AwayScore <= prevInstance.HomeScore && this.AwayScore > this.HomeScore) {
+          str = game.AwayTeam.Name + ' take the lead. ' + scores + this.Time + ' ' + this.Period;
+        } else {
+          str = game.AwayTeam.Name + ' score. ' + scores + this.Time + ' ' + this.Period;
+        }
+      } else if (prevInstance.HomeScore !== this.HomeScore && !isOT) {
+        if (this.AwayScore === this.HomeScore) {
+          str = game.HomeTeam.Name + ' tie it up. ' + scores + this.Time + ' ' + this.Period;
+        } else if (prevInstance.HomeScore <= prevInstance.AwayScore &&
+            this.HomeScore > this.AwayScore) {
+          str = game.HomeTeam.Name + ' take the lead. ' + scores + this.Time + ' ' + this.Period;
+        } else {
+          str = game.HomeTeam.Name + ' score. ' + scores + this.Time + ' ' + this.Period;
+        }
+      }
+    }
+    return str ? (str.trim() + ' ' + game.makeGameLink()) : '';
+  }
+}
 
 Instance.parse = function(raw) {
   const instanceObj = {
@@ -56,68 +118,6 @@ Instance.parse = function(raw) {
       break;
   }
   return new Instance(instanceObj);
-};
-
-Instance.prototype.scoreChanged = function(prevInstance) {
-  return (
-    prevInstance.State !== this.State ||
-    prevInstance.Period !== this.Period ||
-    prevInstance.AwayScore !== this.AwayScore ||
-    prevInstance.HomeScore !== this.HomeScore ||
-    (
-      prevInstance.State === 'Scheduled' &&
-      this.State === 'Scheduled' &&
-      prevInstance.Time !== this.Time &&
-      this.Time && this.Time.length
-    )
-  );
-};
-
-Instance.prototype.changeString = function(prevInstance, game) {
-  let str = '';
-  const scores = game.AwayTeam.Name + ' ' + this.AwayScore + ', ' +
-    game.HomeTeam.Name + ' ' + this.HomeScore + '. ';
-  if (prevInstance.State !== this.State) {
-    if (prevInstance.State === 'Scheduled' && this.State === 'Progress') {
-      if (this.Period !== '1st') {
-        return str;
-      }
-      str = 'Start of game: ' + game.AwayTeam.Name + ' vs ' + game.HomeTeam.Name;
-    }
-    if (this.State === 'Final') {
-      if (this.Period === 'SO') {
-        str = 'Final SO. ' + scores;
-      } else if (this.Period === 'OT') {
-        str = 'Final OT. ' + scores;
-      } else {
-        str = 'Final. ' + scores;
-      }
-    }
-  } else {
-    const isOT = (this.State === 'Overtime' || this.State === 'Shootout');
-    if (prevInstance.AwayScore > this.AwayScore ||
-      prevInstance.HomeScore > this.HomeScore) {
-      str = 'Score Correction. ' + scores + this.Time + ' ' + this.Period;
-    } else if (prevInstance.AwayScore !== this.AwayScore && !isOT) {
-      if (this.AwayScore === this.HomeScore) {
-        str = game.AwayTeam.Name + ' tie it up. ' + scores + this.Time + ' ' + this.Period;
-      } else if (prevInstance.AwayScore <= prevInstance.HomeScore && this.AwayScore > this.HomeScore) {
-        str = game.AwayTeam.Name + ' take the lead. ' + scores + this.Time + ' ' + this.Period;
-      } else {
-        str = game.AwayTeam.Name + ' score. ' + scores + this.Time + ' ' + this.Period;
-      }
-    } else if (prevInstance.HomeScore !== this.HomeScore && !isOT) {
-      if (this.AwayScore === this.HomeScore) {
-        str = game.HomeTeam.Name + ' tie it up. ' + scores + this.Time + ' ' + this.Period;
-      } else if (prevInstance.HomeScore <= prevInstance.AwayScore &&
-          this.HomeScore > this.AwayScore) {
-        str = game.HomeTeam.Name + ' take the lead. ' + scores + this.Time + ' ' + this.Period;
-      } else {
-        str = game.HomeTeam.Name + ' score. ' + scores + this.Time + ' ' + this.Period;
-      }
-    }
-  }
-  return str ? (str.trim() + ' ' + game.makeGameLink()) : '';
 };
 
 module.exports = Instance;
