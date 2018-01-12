82f17df9e32aef79a7329b8cdeb21d7335f4bcfd
Sanders DeNardi <sedenardi@gmail.com>
2018-01-11 21:59:52 -0500

    switch NFL back to all scores

--- models/nfl/Instance.js
+++ models/nfl/Instance.js
@@ -19,7 +19,7 @@ class Instance {
       prevInstance.HomeScore !== this.HomeScore ||
       prevInstance.AwayScore !== this.AwayScore;
   }
-  changeString(prevInstance, game) {
+  leadChangeString(prevInstance, game) {
     let str = '';
     const scores = game.AwayTeam.Name + ' ' + this.AwayScore + ', ' +
       game.HomeTeam.Name + ' ' + this.HomeScore + '. ';
@@ -54,6 +54,32 @@ class Instance {
       }
     }
     return str ? (str.trim() + ' ' + game.makeGameLink()) : '';
+  }
+  allChangeString(prevInstance, game) {
+    let str = '';
+    const scores = game.AwayTeam.Name + ' ' + this.AwayScore + ', ' +
+      game.HomeTeam.Name + ' ' + this.HomeScore + '. ';
+    const time = `${this.Time} ${ordinal(this.Quarter)} quarter.`;
+    if (this.State !== prevInstance.State) {
+      if (prevInstance.State === 'Pregame' && this.State === 'Progress') {
+        str = `Start of game: ${game.AwayTeam.Name} vs ${game.HomeTeam.Name}`;
+      }
+      if (this.State === 'Final') {
+        str = `Final${(prevInstance.Quarter === 'OT' ? ' OT' : '')}. ${scores}`;
+      }
+    } else if ((this.Quarter === prevInstance.Quarter && prevInstance.timeNum() >= this.timeNum()) || (prevInstance.Quarter < this.Quarter)) {
+      if (prevInstance.AwayScore > this.AwayScore || prevInstance.HomeScore > this.HomeScore) {
+        str = `Score correction. ${scores} ${time}`;
+      } else if (prevInstance.AwayScore !== this.AwayScore) {
+        str = `${game.AwayTeam.Name} score. ${scores} ${time}`;
+      } else if (prevInstance.HomeScore !== this.HomeScore) {
+        str = `${game.HomeTeam.Name} score. ${scores} ${time}`;
+      }
+    }
+    return str ? (str.trim() + ' ' + game.makeGameLink()) : '';
+  }
+  changeString(prevInstance, game) {
+    return this.allChangeString(prevInstance, game);
   }
 }
 
